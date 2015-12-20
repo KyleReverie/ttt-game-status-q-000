d3729d694ff7ccf70dc981e334e3292afe277e79
Kyle Reverie <git@ktrex.us>
2015-12-20 18:05:30 +0000

    nitrous-backup at Sun Dec 20 18:05:30 UTC 2015

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,3 +5,12 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [ [0,1,2] , [3,4,5] , [6,7,8] , [0,4,8], [6,4,2], [0,3,6], [1,4,7], [2,5,8]  ]
+
+def won?(board)
+  for each WinCombo in WIN_COMBINATIONS
+    if ( board[WinCombo[0]] == board[WinCombo[1]] && board[WinCombo[1]] == board[WinCombo[2]] ) && position_taken?(board,WinCombo[0]
+      return true
+    end
+  end
+  false
+end
